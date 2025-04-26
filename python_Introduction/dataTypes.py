69eb8b7ec61344d1724ab0e2c0ddc9a38e929a0f
Bobybuu <c43057772@gmail.com>
2025-04-26 15:29:45 +0300

    docs: for organizing and storing data in a way that facilitates efficient access and manipulation

---
+++ python_Introduction/dataTypes.py
@@ -0,0 +1,23 @@
+num1 = 1
+num2 = 2.5
+print(num1, type(num1))
+print(num2, type(num2))
+
+list1 = [ "python", "kenya", "Kuon", 1, 2.5 ] #lists
+print(list1, type(list1))
+print(list1[1])
+print(list1[3])
+
+tuple1 = ("python", 2.5, "grass", 1, 2.3) #Tuples
+print(tuple1)
+print(tuple1[2])
+
+site_name = "Power learn project" #strings
+print(site_name)
+
+set1 = { 11, 11,  12, 12, 13, 14, 15} #sets
+print(set1)
+
+capitalCities = {"Kenya":"Nairobi", "Uganda":"Kampala", "Nigeria":"Lagos"}
+print(capitalCities)
+print(capitalCities["Nigeria"])
